1064ad6126d8da3ac76e3dc6a7bc32aa49913156
Jeremy Meyer <jjman505@gmail.com>
2014-01-29 14:48:41 -0500

    added feed

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,13 @@
 module MainHelper
+  def users_and_projects
+    User.all + Project.all
+  end
+
+  def item_path(o, id)
+    if o.is_a?(User)
+      user_path(id)
+    elsif o.is_a?(Project)
+      project_path(id)
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  def name
+    firstName + " " + lastName
+  end
+
+  def description
+    bio
+  end
 end
